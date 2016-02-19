2c76c9d0a4f3e9ba60f00a2a9f511f442bb17d19
falcjohnny <johnny.wu@falconstor.com>
2016-02-19 15:33:15 +0800

    test

--- test_create_del_snap_firefox.py
+++ test_create_del_snap_firefox.py
@@ -137,7 +137,7 @@ class TestCreateDelSnapFirefox(unittest.TestCase):
         #    time.sleep(1)
         #else: self.fail("time out")
 	driver.find_element_by_xpath("//div[@col='0']").click()
-        # Delet SAN Resource
+        # Delet SAN Resource.
     	driver.find_element_by_xpath("//button[contains(@data-template-url,'views/manage/delete-device.tpl.html')]").click()
 	driver.find_element_by_xpath("//a[contains(.,'Delete Selected')]").click()
         for i in range(60):
